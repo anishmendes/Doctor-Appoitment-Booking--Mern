564123ac671159808e79e6dbff1724f193da7924
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-03-12 12:59:21 +0545

    Update Doctors.jsx

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -71,23 +71,7 @@ const Doctors =  () => {
          </section>
        
 
-           {/* <section>
-            <div className="container">
-                <div className="xl:w-[470px] mx-auto">
-                    <h2 className="heading text-center">
-                        What our patient say
-
-                    </h2>
-                    <p className='text_para text-center'>
-                        World class care for everyone. Our health system offers unmatched,
-                        expert health care.
-                    </p>
-                </div>
-
-               <Testimonial/>
-
-            </div>
-           </section> */}
+          
 
 
          
